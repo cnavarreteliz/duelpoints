741bd51a224d3027e1bc56636911055a2ac7b147
nicoperezm <nicolasperezmundaca@Nicolass-MacBook-Air.local>
2019-07-24 17:51:26 -0400

    commit 2.0

---
+++ build/precache-manifest.c1f4758c4a58aa83775f8510c596283d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2b9d9b90b9fba6402633a451fcf76e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc5080127a6db1433d0",
+    "url": "/static/css/2.4340cdf1.chunk.css"
+  },
+  {
+    "revision": "c8474985437db19cbcdb",
+    "url": "/static/css/main.77c8e412.chunk.css"
+  },
+  {
+    "revision": "abc5080127a6db1433d0",
+    "url": "/static/js/2.20ee15af.chunk.js"
+  },
+  {
+    "revision": "c8474985437db19cbcdb",
+    "url": "/static/js/main.9e82aa63.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
